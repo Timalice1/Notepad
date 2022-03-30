9e0e8f6c451cb79c133fdbc2bb15d7395dca2939
Timalice1 <newacccc2@gmail.com>
2022-03-30 16:27:12 +0300

    initial commit

--- Notepad/Form1.cs
+++ Notepad/Form1.cs
@@ -15,17 +15,24 @@ namespace Notepad {
 
         private void menuCreateNew_Click(object sender, EventArgs e) {
             var tab = new TabPage("tabPage");
+            tab.Font = new System.Drawing.Font("Calibri", 10);
             tabControl1.SelectedTab = tab;
             tab.BorderStyle = BorderStyle.FixedSingle;
             var textBox = new TextField(tab);
             tabControl1.TabPages.Add(tab);
             menuSaveAs.Enabled = true;
             textBox.IsSaved = false;
+            textBox.TextChanged += new EventHandler(textBox_TextChanged);
 
             textFields.Add(textBox);
         }
 
         private void menuClose_Click(object sender, EventArgs e) {
+            foreach(var textField in textFields) {
+                if (!textField.IsSaved) {
+                    MessageBox.Show($"File {textField.Parent.Text} has unsaved changes");
+                }
+            }
             textFields.RemoveAt(tabControl1.SelectedIndex);
             tabControl1.TabPages.Remove(tabControl1.SelectedTab);
         }
@@ -43,10 +50,14 @@ namespace Notepad {
             string filePath = dialog.FileName;
 
             var tab = new TabPage(filePath);
+            tab.Font = new System.Drawing.Font("Calibri", 10);
+
+
             tabControl1.SelectedTab = tab;
             tabControl1.TabPages.Add(tab);
             tab.BorderStyle = BorderStyle.FixedSingle;
             var textBox = new TextField(tab);
+            textBox.TextChanged += new EventHandler(textBox_TextChanged);
 
             StreamReader sr = new StreamReader(filePath);
             textBox.Text = sr.ReadToEnd();
@@ -72,8 +83,9 @@ namespace Notepad {
         }
 
         private void menuSave_Click(object sender, EventArgs e) {
-            Save(tabControl1.SelectedTab.Text);
-            
+            var path = tabControl1.SelectedTab.Text.Remove(tabControl1.SelectedTab.Text.LastIndexOf(" "));
+            Save(path);
+            tabControl1.SelectedTab.Text = path;
         }
 
         private void menuSaveAs_Click(object sender, EventArgs e) {
@@ -87,6 +99,13 @@ namespace Notepad {
 
         }
         
+        private void textBox_TextChanged(object sender, EventArgs e){
+            textFields[tabControl1.SelectedIndex].IsSaved = false;
+            if (textFields[tabControl1.SelectedIndex].Parent.Text.Contains("*"))
+                return;
+
+            textFields[tabControl1.SelectedIndex].Parent.Text += " *";
+        }
 
     }
 }
